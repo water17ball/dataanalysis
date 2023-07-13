c6a4f719518aedac826e6f611f5cd873a685f539
wangpei <wangpei34@xdf.cn>
2023-07-13 16:01:54 +0800

    添加注释

--- src/main/java/cn/enn/bigdata/dataanalysis/service/dealchain/DealChain.java
+++ src/main/java/cn/enn/bigdata/dataanalysis/service/dealchain/DealChain.java
@@ -8,6 +8,12 @@ import javax.annotation.Resource;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * 处理链类
+ * 1.包含多个处理类，每个处理继承DealProcess虚基类，实现dealCurrent具体方法
+ * 2.处理类中具有执行顺序index，可按顺序执行处理； 具有终结属性，如果是终结处理，则执行完成后停止处理。（可以像积木一样串行编排处理任务）
+ * 3.ProcessContent处理内容：data为模板T类型
+ */
 @Component
 public class DealChain {
 
